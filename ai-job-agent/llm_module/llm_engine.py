1c2060bc99a2260ffeb99940d87e37e787a938b4
Prince Kumar <prince.kumar226259@gmail.com>
2026-03-29 20:12:26 +0530

    Update llm_engine.py

--- ai-job-agent/llm_module/llm_engine.py
+++ ai-job-agent/llm_module/llm_engine.py
@@ -22,3 +22,57 @@ model = ChatGoogleGenerativeAI(
     temperature=0.3,
     google_api_key=api_key
 )
+
+
+def improve_resume(resume_text, job_description):
+
+    prompt = f"""
+You are an expert AI resume coach.
+
+Improve this resume based on the job description.
+
+Focus on:
+- Better bullet points
+- Add missing skills
+- ATS-friendly
+
+Resume:
+{resume_text}
+
+Job Description:
+{job_description}
+
+Return improved bullet points only.
+"""
+
+    try:
+        response = model.invoke(prompt)
+        return response.content
+    except Exception as e:
+        return f"⚠️ AI Error: {str(e)}"
+
+
+def generate_cover_letter(resume_text, job_description):
+
+    prompt = f"""
+Write a professional cover letter.
+
+Make it:
+- concise
+- personalized
+- impactful
+
+Resume:
+{resume_text}
+
+Job Description:
+{job_description}
+"""
+
+    try:
+        response = model.invoke(prompt)
+    
+        return response.content
+    except Exception as e:
+        return f"⚠️ AI Error: {str(e)}"
+
